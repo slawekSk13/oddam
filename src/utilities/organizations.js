16d13f5c1f7a8708e1bfa9b2b83e46c8221edd07
slawekSk13 <slawomir.skrzeczynski@gmail.com>
2021-08-09 15:56:53 +0200

    populated organisations

---
+++ src/utilities/organizations.js
@@ -0,0 +1,98 @@
+const fundations = [
+    {
+        name: 'Fundacja "Dbam o zdrowie',
+        goal: 'Pomoc osobom znajdującym się w trudnej sytuacji życiowej',
+        needs: 'ubrania, jedzenie, sprzęt AGD, meble, zabawki'
+    },
+    {
+        name: 'Fundacja "Dla dzieci',
+        goal: 'Pomoc dzieciom z ubogich rodzin',
+        needs: 'ubrania, meble, zabawki'
+    },
+    {
+        name: 'Fundacja "Bez domu',
+        goal: 'Pomoc dla osób nie posiadających miejsca zamieszkania',
+        needs: 'ubrania, jedzenie, ciepłe koce'
+    },
+    {
+        name: 'Fundacja "Dbam o zdrowie 2',
+        goal: 'Pomoc osobom znajdującym się w trudnej sytuacji życiowej',
+        needs: 'ubrania, jedzenie, sprzęt AGD, meble, zabawki'
+    },
+    {
+        name: 'Fundacja "Dla dzieci 2',
+        goal: 'Pomoc dzieciom z ubogich rodzin',
+        needs: 'ubrania, meble, zabawki'
+    },
+    {
+        name: 'Fundacja "Bez domu 2',
+        goal: 'Pomoc dla osób nie posiadających miejsca zamieszkania',
+        needs: 'ubrania, jedzenie, ciepłe koce'
+    },
+    {
+        name: 'Fundacja "Dbam o zdrowie 3',
+        goal: 'Pomoc osobom znajdującym się w trudnej sytuacji życiowej',
+        needs: 'ubrania, jedzenie, sprzęt AGD, meble, zabawki'
+    },
+    {
+        name: 'Fundacja "Dla dzieci 3',
+        goal: 'Pomoc dzieciom z ubogich rodzin',
+        needs: 'ubrania, meble, zabawki'
+    },
+    {
+        name: 'Fundacja "Bez domu 3',
+        goal: 'Pomoc dla osób nie posiadających miejsca zamieszkania',
+        needs: 'ubrania, jedzenie, ciepłe koce'
+    }
+];
+const ngos = [
+    {
+        name: 'Organizacja "Dbam o zdrowie',
+        goal: 'Pomoc osobom znajdującym się w trudnej sytuacji życiowej',
+        needs: 'ubrania, jedzenie, sprzęt AGD, meble, zabawki'
+    },
+    {
+        name: 'Organizacja "Dla dzieci',
+        goal: 'Pomoc dzieciom z ubogich rodzin',
+        needs: 'ubrania, meble, zabawki'
+    },
+    {
+        name: 'Organizacja "Bez domu',
+        goal: 'Pomoc dla osób nie posiadających miejsca zamieszkania',
+        needs: 'ubrania, jedzenie, ciepłe koce'
+    },
+    {
+        name: 'Organizacja "Dbam o zdrowie 2',
+        goal: 'Pomoc osobom znajdującym się w trudnej sytuacji życiowej',
+        needs: 'ubrania, jedzenie, sprzęt AGD, meble, zabawki'
+    },
+    {
+        name: 'Organizacja "Dla dzieci 2',
+        goal: 'Pomoc dzieciom z ubogich rodzin',
+        needs: 'ubrania, meble, zabawki'
+    },
+    {
+        name: 'Organizacja "Bez domu 2',
+        goal: 'Pomoc dla osób nie posiadających miejsca zamieszkania',
+        needs: 'ubrania, jedzenie, ciepłe koce'
+    }
+];
+const localOrganizations = [
+    {
+        name: 'Organizacja lokalna "Dbam o zdrowie',
+        goal: 'Pomoc osobom znajdującym się w trudnej sytuacji życiowej',
+        needs: 'ubrania, jedzenie, sprzęt AGD, meble, zabawki'
+    },
+    {
+        name: 'Organizacja lokalna "Dla dzieci',
+        goal: 'Pomoc dzieciom z ubogich rodzin',
+        needs: 'ubrania, meble, zabawki'
+    },
+    {
+        name: 'Organizacja lokalna "Bez domu',
+        goal: 'Pomoc dla osób nie posiadających miejsca zamieszkania',
+        needs: 'ubrania, jedzenie, ciepłe koce'
+    }
+];
+
+export {fundations, ngos, localOrganizations}
